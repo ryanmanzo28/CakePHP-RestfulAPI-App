2e28f08f473aa14903694647f5a5a8e0f6d282c0
Ryan Manzo <ryan.manzo@hydracor.net>
2026-06-30 15:51:13 -0400

    began work on authentication middleware

--- api/middleware/AuthenticationMiddleware.php
+++ api/middleware/AuthenticationMiddleware.php
@@ -1,2 +1,39 @@
 <?php
-// AuthenticationMiddleware moved from api/old-api/middleware
+namespace app\controller\api;
+use firebase\JWT\JWT;
+
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        $user = $this->Users
+        ->find()
+        ->where(['email' => $this->request->getData('email')])
+            ->first();
+
+        if (!$user) {
+            return $this->response->withStatus(401);
+        }
+
+        $payload = [
+            'sub' => $user->id,
+            'exp' => time() + 3600, // Token expires in 1 hour
+
+        ];
+        $token = JWT::encode(
+            $payload,
+            'secret-key',
+            HS256_KEY,
+
+        );
+        return $this->response->withType('application/json')
+            ->withStringBody(json_encode([
+                'token' => $token,
+            ]));
+        
+    }
+
+}
+
+
